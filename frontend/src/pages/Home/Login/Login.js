79fe9b2ae0df0a9d99036fd90f95a6bf9c3c05bc
ranvi2006 <rajurk2002@gmail.com>
2024-07-17 11:17:03 +0530

    added css file of login and signup

--- frontend/src/pages/Home/Login/Login.js
+++ frontend/src/pages/Home/Login/Login.js
@@ -1,28 +1,28 @@
-import React, { useState  } from 'react'
+import React, { useState } from 'react'
 import tituImage from '../../../assets/images/spidoImage.jpg'
 import BlurOnIcon from '@mui/icons-material/BlurOn';
-import {   useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import auth from '../../../firebase.init';
 import './Login.css';
 
 
 export default function Login() {
 
-    const [email ,setEmail]=useState('');
-    const [password,setPassword]=useState('');
-    // const [error,setError]=useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  // const [error,setError]=useState('');
 
 
-    //-----------------------------------
-  const [ 
+  //-----------------------------------
+  const [
     SignInWithEmailAndPassword,
     user,
     loading,
     error
-    
-]
-=useSignInWithEmailAndPassword(auth);
-if (error) {
+
+  ]
+    = useSignInWithEmailAndPassword(auth);
+  if (error) {
     return (
       <div>
         <p>Error: {error.message}</p>
@@ -41,42 +41,45 @@ if (error) {
   }
   //---------------------------------
 
-    const handleSubmit = e=>
-    {
-e.preventDefault();
-console.log(email , password);
-SignInWithEmailAndPassword(email,password);
-    }
-    return (
-        <>
-            <div className="loginContainer">
-              {/* g1 */}
-                <div className="container">
-                    <img clasName='mainImage'  src={tituImage} alt='/' />
-                </div> 
-                {/* g2 */}
-                <div className="formContainer">
-                    <BlurOnIcon />
-                    <h4>Raju</h4>
-                    <form onSubmit={handleSubmit}>
-                        
-                        <input type="email" 
-                        className='email'
-                        placeholder='Email address'
-                        onChange={(e)=>setEmail(e.target.value)}
-                        />
-                        
-                        <input type="password" 
-                        className="pasward"
-                        placeholder='password'
-                        onChange={(e)=>setPassword(e.target.value)}
-                        />
-                        <div>
-                            <button type="submit" className='btn'>login</button>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </>
-    )
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log(email, password);
+    SignInWithEmailAndPassword(email, password);
+  }
+  return (
+    <>
+      <div className="loginContainer">
+        {/* g1 */}
+        <div className="container">
+          <div className="logoContainer">
+            <img className='logo' src='https://png.pngtree.com/png-clipart/20230823/original/pngtree-spider-logo-template-vector-icon-illustration-design-picture-image_8298508.png' alt="" />
+          </div>
+          {/* <img className='mainImage'  src={tituImage} alt='/' /> */}
+
+          <div className="formContainer">
+
+            <form className='form' onSubmit={handleSubmit}>
+              <h1>Welcome Spido-Family</h1>
+
+              <input type="email"
+                className='email'
+                placeholder='Email address'
+                onChange={(e) => setEmail(e.target.value)}
+              />
+
+              <input type="password"
+                className="pasward"
+                placeholder='password'
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <button type="submit" className='login'>login</button>
+             
+
+            </form>
+          </div>
+        </div>
+        {/* g2 */}
+      </div>
+    </>
+  )
 }
--- frontend/src/pages/Home/Signup/Signup.js
+++ frontend/src/pages/Home/Signup/Signup.js
@@ -6,94 +6,99 @@ import auth from '../../../firebase.init';
 import GoogleButton from 'react-google-button';
 import Login from '../Login/Login';
 import { Link } from 'react-router-dom';
+import './Signup.css';
 
 export default function Signup() {
-    const [userName,setUserName]=useState('');
-    const [fullName,setFullName]=useState('');
-    const [email,setEmail]=useState('');
-    const [password,setPassword]=useState('');
-    //------------------------------------
-    const [
-        createSignUpWithEmailAndPassword,
-        user,
-        loading,
-        error
-    ]=useCreateUserWithEmailAndPassword(auth);
-    if (error) {
-        return (
-          <div>
-            <p>Error: {error.message}</p>
-          </div>
-        );
-      }
-      if (loading) {
-        return <p>Loading...</p>;
-      }
-      if (user) {
-        return (
-          <div>
-            <p>Signed In User: {user.email}</p>
-          </div>
-        );
-      }
-      //----------------------------------------------
+  const [userName, setUserName] = useState('');
+  const [fullName, setFullName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  //------------------------------------
+  const [
+    createSignUpWithEmailAndPassword,
+    user,
+    loading,
+    error
+  ] = useCreateUserWithEmailAndPassword(auth);
+  if (error) {
+    return (
+      <div>
+        <p>Error: {error.message}</p>
+      </div>
+    );
+  }
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+  if (user) {
+    return (
+      <div>
+        <p>Signed In User: {user.email}</p>
+      </div>
+    );
+  }
+  //----------------------------------------------
 
-    const handleSubmit=(e)=>
-    {
-e.preventDefault();
-console.log(fullName,userName,email,password);
-createSignUpWithEmailAndPassword(email,password);
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(fullName, userName, email, password);
+    createSignUpWithEmailAndPassword(email, password);
+  }
 
   return (
-   <>
-   <div className="signupConatiner">
-    <div className="container">
-        <img src={tituImage} alt='image'/>
-    </div>
-    <div className="formContainer">
-              <BlurOnIcon/>
-              <form onSubmit={handleSubmit}>
-                <input type='text'
+    <>
+      <div className="signupConatiner">
+        <div className="container">
+            <div className="logoContainer">
+              <img className='logo' src='https://png.pngtree.com/png-clipart/20230823/original/pngtree-spider-logo-template-vector-icon-illustration-design-picture-image_8298508.png' alt="" />
+            </div>
+          <div className="formContainer">
+            <form className="form" onSubmit={handleSubmit}>
+            <h1>Welcome To Spido-World</h1>
+              <input type='text'
                 className='userName'
                 placeholder='userName'
-                onChange={(e)=>setUserName(e.target.value)}
-                />
-                <input type='text'
+                onChange={(e) => setUserName(e.target.value)}
+              />
+              <input type='text'
                 className='fullName'
                 placeholder='fullName'
-                onChange={(e)=>setFullName(e.target.value)}
-                />
-                <input type='email'
+                onChange={(e) => setFullName(e.target.value)}
+              />
+              <input type='email'
                 className='email'
                 placeholder='Email id'
-                onChange={(e)=>setEmail(e.target.value)}
-                />
-                <input type='password'
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <input type='password'
                 className='password'
                 placeholder='password'
-                onChange={(e)=>setPassword(e.target.value)}
-                />
-                <div>
-                    <button className='btn' type='submit'>submit</button>
-                </div>
-                <br />
-                <div className='google-button'>
-                     <GoogleButton  className='g-btn'
-                     type='light'/>
-                </div>
-                <div className='another'>
-                    already have an account..
-                <Link 
-                to='/login' style={{
-                    textDecoration:'none',
-                    color:'skybule'}}>login</Link>
-                </div>
-                 
-              </form>
-    </div>
-   </div>
-   
-   </>
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <div>
+                <button className='btn' type='submit'>submit</button>
+              </div>
+              <br />
+              <div className='google-button'>
+                <GoogleButton className='g-btn'
+                  type='light' />
+              </div>
+              <div className='another'>
+            already have an account?
+            <Link
+              to='/login' style={{
+                textDecoration: 'none',
+                color: 'skybule'
+              }}><b className='login'>login</b></Link>
+          </div>
+          </form>
+
+          </div>
+          
+        </div>
+
+      </div>
+
+    </>
   )
 }
